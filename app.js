972a23ea973666518588b3042b6f4e7c94f38431
Olha Dudchenko <dudchenko.olha92@gmail.com>
2021-12-31 14:12:52 +0200

    add code

---
+++ app.js
@@ -0,0 +1,30 @@
+const board = document.querySelector("#board")
+const SQUARE_NUMBERS = 500;
+
+for (let i = 0; i < SQUARE_NUMBERS; i += 1){
+    const square = document.createElement("div")
+    square.classList.add("square")
+    square.addEventListener("mouseover", () => {
+    setColor(square)
+    })
+       square.addEventListener("mouseleave", () => {
+    removeColor(square)
+})
+    board.append(square)
+    
+}
+
+function setColor(element) {
+    const color = `rgb(${randomRgb(0, 255)},${randomRgb(0, 255)},${randomRgb(0, 255)})`
+    element.style.backgroundColor = color
+    element.style.boxShadow=`0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = "#1d1d1d"
+     element.style.boxShadow=`0 0 2px #000`
+}
+
+function randomRgb(min,max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
